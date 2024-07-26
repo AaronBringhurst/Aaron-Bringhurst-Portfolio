466c39d58d2e3d903356690078c42b5dcce8b981
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-07-25 21:12:52 -0600

    added images for my projects, and build out the descriptions

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -4,33 +4,39 @@ const Portfolio = () => {
     const projects = [
       {
         id: 1,
-        title: "Project 1",
-        description: "A brief description of project 1",
+        title: "Dystopi-Track",
+        description:
+          "DystopiTrack Systems is a command-line interface (CLI) application designed to manage employee data within corporate environment. The application offers a rich set of features allowing administrators to handle various aspects of employee, department, and role management efficiently, directly from the terminal.",
       },
       {
         id: 2,
-        title: "Project 2",
-        description: "A brief description of project 2",
+        title: "Code Nest",
+        description:
+          "Code Nest is a dynamic web application designed to foster a community of developers and tech enthusiasts. It provides a platform for users to share, discuss, and collaborate on crypto-related topics. With features like user authentication, post creation, and commenting, Code Nest aims to be a hub for knowledge sharing in the tech world.",
       },
       {
         id: 3,
-        title: "Project 3",
-        description: "A brief description of project 3",
+        title: "WebPack-Text-O-Tron",
+        description:
+          "WebPack-Text-O-Tron is a progressive web application that functions as a text editor. It allows users to create and edit text content with JavaScript syntax highlighting. The app works both online and offline, providing a seamless experience for developers who need a reliable text editor.",
       },
       {
         id: 4,
-        title: "Project 4",
-        description: "A brief description of project 3",
+        title: "Aarons Blog",
+        description:
+          "a place for sharing and reading tales from various trails around the globe. This project encompasses a simple, responsive blog site designed for users to share their travel stories and experiences.",
       },
       {
         id: 5,
-        title: "Project 5",
-        description: "A brief description of project 3",
+        title: "Game of Loans",
+        description:
+          "A dynamic budgeting application designed to help you manage your finances with ease and insight. Tailored for both financial novices and savvy savers, BudgetMaster offers an intuitive and responsive interface to make budgeting a straightforward and engaging process.",
       },
       {
         id: 6,
-        title: "Project 6",
-        description: "A brief description of project 3",
+        title: "Trying to Sell",
+        description:
+          "The Trying to Sell project is a comprehensive website designed to facilitate the online selling of various products. This server-side application manages product inventories, categories, and tags through a RESTful API. It demonstrates the integration of Express.js with Sequelize ORM for database management using PostgreSQL.",
       },
     ];
 
